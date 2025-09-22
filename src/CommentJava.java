cb6685b5b70d820426fc8a81bd641750842e94f0
joouseong <joowsooy@gmail.com>
2025-09-22 22:46:48 +0900

    first commit

---
+++ src/CommentJava.java
@@ -0,0 +1,17 @@
+public class CommentJava {
+
+    public static void main(String[] args) { //psvm 이라는 단축어를 사용하여 편리하게 생성
+        System.out.println("Hello java1"); //sout이라는 단축어 사용 //hello java1을 출력합니다
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+
+        // 한 줄 주석(single line comment)
+        /*
+           여러 줄 주석(multi line comment)
+        */
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        //System.out.println(a); //변수 초기화를 하지 않고 실행하면 컴파일 에러 발생
+    }
+}
